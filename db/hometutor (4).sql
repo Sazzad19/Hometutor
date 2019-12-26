a3f18df546f9e935ce09e83cb6ca59223e39fa0a
Sazzad19 <shakilahmedzx@gmail.com>
2019-12-26 21:02:59 +0600

    5th commit(db)

--- db/hometutor (4).sql
+++ db/hometutor (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2019 at 07:58 PM
+-- Generation Time: Dec 26, 2019 at 03:56 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -30,9 +30,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `guardianreviews` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `guardian_id` int(255) NOT NULL,
+  `ratting` int(11) NOT NULL,
+  `feedback` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `guardianreviews`
+--
+
+INSERT INTO `guardianreviews` (`id`, `guardian_id`, `ratting`, `feedback`, `created_at`, `updated_at`) VALUES
+(1, 12, 8, 'Good behaviour and timely payment', '2019-12-24 11:48:26', '2019-12-24 11:48:26'),
+(2, 12, 5, 'Late Payment but Good behaviour.', '2019-12-24 11:55:20', '2019-12-24 11:55:20'),
+(3, 12, 6, 'Overall Good', '2019-12-24 12:37:41', '2019-12-24 12:37:41'),
+(4, 11, 8, 'Good', '2019-12-25 03:04:47', '2019-12-25 03:04:47'),
+(5, 11, 7, 'Lately Payment', '2019-12-25 03:07:44', '2019-12-25 03:07:44');
 
 -- --------------------------------------------------------
 
@@ -45,9 +59,9 @@ CREATE TABLE `guardians` (
   `user_id` int(255) NOT NULL,
   `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `photo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profession` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone_number` int(11) NOT NULL,
-  `children` int(11) NOT NULL,
+  `phone_number` int(20) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -56,9 +70,10 @@ CREATE TABLE `guardians` (
 -- Dumping data for table `guardians`
 --
 
-INSERT INTO `guardians` (`id`, `user_id`, `username`, `photo`, `address`, `phone_number`, `children`, `created_at`, `updated_at`) VALUES
-(11, 11, 'masum420', '1576520136.jpg', 'Maijdee,Noakhali', 1789477764, 2, '2019-12-16 12:15:36', '2019-12-16 12:15:36'),
-(12, 17, 'taifur20', '1576566142.jpg', 'Maijdee,Noakhali', 1789477764, 2, '2019-12-17 01:02:22', '2019-12-17 01:02:22');
+INSERT INTO `guardians` (`id`, `user_id`, `username`, `photo`, `profession`, `address`, `phone_number`, `created_at`, `updated_at`) VALUES
+(11, 11, 'masum420', '1576520136.jpg', '', 'Maijdee,Noakhali', 1789477764, '2019-12-16 12:15:36', '2019-12-16 12:15:36'),
+(12, 17, 'taifur20', '1576566142.jpg', 'Government Employee', 'Madhabpur.Habiganj', 1234567894, '2019-12-17 01:02:22', '2019-12-25 03:20:52'),
+(14, 36, 'aakil14', '1577127900.jpg', 'Businessman', 'Habiganj,Sylhet', 1789477764, '2019-12-23 13:05:01', '2019-12-23 13:05:01');
 
 -- --------------------------------------------------------
 
@@ -111,7 +126,7 @@ CREATE TABLE `students` (
   `photo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gender` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `class` int(11) NOT NULL,
-  `medium` int(191) NOT NULL,
+  `medium` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `educational_institution` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `academic_result` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `guardian_id` int(11) DEFAULT NULL,
@@ -125,9 +140,9 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `username`, `email`, `photo`, `gender`, `class`, `medium`, `educational_institution`, `academic_result`, `guardian_id`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Sakib', 'sakib430', 'saku9628@gmail.com', '1568994820.jpg', 'male', 10, 1, 'NSTU', 'J.S.C: A+', 1, '$2y$10$nuzgcBfHnWbxRL0GFKT/P.5cZCkHKWjVZPMaQm/SULmGIN0MrG7Om', '2019-09-20 09:53:40', '2019-09-20 09:53:40'),
-(2, 'Masum', 'masum420', 'aakilaslamas@gmail.com', '1568995355.jpg', 'male', 10, 2, 'NSTU', 'J.S.C: A+', 2, '$2y$10$ZR8motphMA33skNgwq7jcuobaYhEDoHKcvM1cHM2yhgrK76XoOd7y', '2019-09-20 10:02:37', '2019-09-20 10:02:37'),
-(3, 'Sazzad Shakil', 'sazzad19', 'shakilahmedzx@gmail.com', '1568996158.jpg', 'male', 10, 2, 'NSTU', 'J.S.C: A+', 1, '$2y$10$x0KVniHfKEEg1ZZGi0fX5um2cPM1MNgxuFIKXGphm7wiUqotWc5pS', '2019-09-20 10:15:58', '2019-09-20 10:15:58');
+(1, 'Sakib', 'sakib430', 'saku9628@gmail.com', '1568994820.jpg', 'male', 10, 'Bangla', 'NSTU', 'J.S.C: A+', 11, '$2y$10$nuzgcBfHnWbxRL0GFKT/P.5cZCkHKWjVZPMaQm/SULmGIN0MrG7Om', '2019-09-20 09:53:40', '2019-09-20 09:53:40'),
+(2, 'Masum', 'masum420', 'aakilaslamas@gmail.com', '1568995355.jpg', 'male', 10, 'English', 'NSTU', 'J.S.C: A+', 12, '$2y$10$ZR8motphMA33skNgwq7jcuobaYhEDoHKcvM1cHM2yhgrK76XoOd7y', '2019-09-20 10:02:37', '2019-09-20 10:02:37'),
+(3, 'Sazzad Shakil', 'sazzad19', 'shakilahmedzx@gmail.com', '1568996158.jpg', 'male', 10, 'English', 'NSTU', 'J.S.C: A+', 14, '$2y$10$x0KVniHfKEEg1ZZGi0fX5um2cPM1MNgxuFIKXGphm7wiUqotWc5pS', '2019-09-20 10:15:58', '2019-09-20 10:15:58');
 
 -- --------------------------------------------------------
 
@@ -203,9 +218,21 @@ INSERT INTO `tuitions` (`id`, `title`, `s_fullName`, `s_gender`, `s_college`, `s
 
 CREATE TABLE `tutorreviews` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `ratting` int(11) NOT NULL,
+  `feedback` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tutorreviews`
+--
+
+INSERT INTO `tutorreviews` (`id`, `tutor_id`, `ratting`, `feedback`, `created_at`, `updated_at`) VALUES
+(1, 36, 8, 'Talented Tutor', '2019-12-25 03:15:06', '2019-12-25 03:15:06'),
+(2, 29, 7, 'Good Tutor', '2019-12-25 03:17:47', '2019-12-25 03:17:47'),
+(3, 28, 7, 'Not Regular', '2019-12-25 05:02:31', '2019-12-25 05:02:31');
 
 -- --------------------------------------------------------
 
@@ -222,7 +249,8 @@ CREATE TABLE `tutors` (
   `areas` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `educational_qualification` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `profession` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_card` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_card_front_part` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_card_back_part` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
   `experience_of_tuition` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `current_tuition` int(11) DEFAULT NULL,
   `available_start_time` time DEFAULT NULL,
@@ -237,11 +265,11 @@ CREATE TABLE `tutors` (
 -- Dumping data for table `tutors`
 --
 
-INSERT INTO `tutors` (`id`, `user_id`, `username`, `photo`, `gender`, `areas`, `educational_qualification`, `profession`, `id_card`, `experience_of_tuition`, `current_tuition`, `available_start_time`, `available_end_time`, `expert_in`, `phone_number`, `created_at`, `updated_at`) VALUES
-(28, 14, 'Sazzad19', '1576564737.jpg', 'male', 'Sylhet', 'B.SC.(Running)', 'Student', '', '2 years', 2, '19:00:00', '21:00:00', 'ICT', 1789477764, '2019-12-17 00:38:57', '2019-12-17 00:38:57'),
-(29, 16, 'sharmin20', '1576565311.jpg', 'male', 'Jassore', 'B.SC.(Running)', 'Student', '', '2 years', 2, '19:00:00', '21:00:00', 'Mathemetics', 1789477764, '2019-12-17 00:48:31', '2019-12-17 00:48:31'),
-(30, 18, 'Sohelimam', '1576569425.jpg', 'male', 'Sylhet', 'B.S.C', 'Teacher', '', '2 years', 2, '19:00:00', '21:00:00', 'ICT', 1789477764, '2019-12-17 01:57:06', '2019-12-17 01:57:06'),
-(33, 27, 'salah420', '1576684550.jpg', 'male', 'Sylhet', 'B.SC.(Running)', 'Teacher', 'C:\\xampp\\tmp\\phpF903.tmp', '2 years', 2, '19:00:00', '21:00:00', 'Mathemetics', 1789477764, '2019-12-18 09:55:50', '2019-12-18 09:55:50');
+INSERT INTO `tutors` (`id`, `user_id`, `username`, `photo`, `gender`, `areas`, `educational_qualification`, `profession`, `id_card_front_part`, `id_card_back_part`, `experience_of_tuition`, `current_tuition`, `available_start_time`, `available_end_time`, `expert_in`, `phone_number`, `created_at`, `updated_at`) VALUES
+(28, 14, 'Sazzad19', '1576564737.jpg', 'male', 'Sylhet', 'B.SC.(Running)', 'Student', '', '', '2 years', 2, '19:00:00', '21:00:00', 'ICT', 1789477764, '2019-12-17 00:38:57', '2019-12-17 00:38:57'),
+(29, 16, 'sharmin20', '1576565311.jpg', 'male', 'Jassore', 'B.SC.(Running)', 'Student', '', '', '2 years', 2, '19:00:00', '21:00:00', 'Mathemetics', 1789477764, '2019-12-17 00:48:31', '2019-12-17 00:48:31'),
+(30, 18, 'Sohelimam', '1576569425.jpg', 'male', 'Sylhet', 'B.S.C', 'Teacher', '', '', '2 years', 2, '19:00:00', '21:00:00', 'ICT', 1789477764, '2019-12-17 01:57:06', '2019-12-17 01:57:06'),
+(36, 33, 'saif10', '1577005755.jpg', 'male', 'Sylhet', 'B.SC.', 'Employee', '254590146.JPG', '1901642656.JPG', '1 year', 1, '19:00:00', '21:00:00', 'English', 1789477764, '2019-12-22 03:09:17', '2019-12-25 03:50:35');
 
 -- --------------------------------------------------------
 
@@ -265,12 +293,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(11, 'Masum Billah', 'saku9628@gmail.com', NULL, '$2y$10$JLa8dX3yxY47/Qw9gCG3l...Eo5QF/hsMD84vKFS34w4hZynOizvO', NULL, '2019-12-16 12:15:35', '2019-12-16 12:15:35'),
-(14, 'Sazzad Shakil', 'aakilaslam@gmail.com', NULL, '$2y$10$ysCkwjWayeAh00.rclSpc.9KufdE2ZpWKUCzRV2DOTZkCm/ab7LHq', NULL, '2019-12-17 00:38:57', '2019-12-17 00:38:57'),
+(11, 'Masum Billah', 'saifuzzal90@gmail.com', NULL, '$2y$10$JLa8dX3yxY47/Qw9gCG3l...Eo5QF/hsMD84vKFS34w4hZynOizvO', NULL, '2019-12-16 12:15:35', '2019-12-16 12:15:35'),
+(14, 'Sazzad Shakil', 'saifuzzal@gmail.com', NULL, '$2y$10$ysCkwjWayeAh00.rclSpc.9KufdE2ZpWKUCzRV2DOTZkCm/ab7LHq', NULL, '2019-12-17 00:38:57', '2019-12-17 00:38:57'),
 (16, 'Sharmin', 'shakilahmedzx@gmail.com', NULL, '$2y$10$66gyKUzNS3CrVdQP.eKZteA1NKUl6HWzR8MqTOPf3WdbSpBl/Rdhi', NULL, '2019-12-17 00:48:31', '2019-12-17 00:48:31'),
-(17, 'Taifur Ahmed', 'taifur@gmail.com', NULL, '$2y$10$uhLQI33zcLIFJl9hc2R0ku77KbimeHw91XFOf7LHBf1j8fMsMJuIW', NULL, '2019-12-17 01:02:21', '2019-12-17 01:02:21'),
+(17, 'Taifur Ahmed', 'taifur@gmail.com', NULL, '$2y$10$uhLQI33zcLIFJl9hc2R0ku77KbimeHw91XFOf7LHBf1j8fMsMJuIW', NULL, '2019-12-17 01:02:21', '2019-12-25 03:20:52'),
 (18, 'Soheli Mam', 'sohelisoha@gmail.com', NULL, '$2y$10$V7k.Bd7DUNeJf.dV1lGLo.vN1AC687ZyLAJsid2UHRcjnLKXB1kh6', NULL, '2019-12-17 01:57:05', '2019-12-17 01:57:05'),
-(27, 'Salah Uddin', 'naylamerrry40720@gmail.com', NULL, '$2y$10$bRtxUNkeGPJT3Hhb5CX4Y.OfNN6xQ4Bjp/.eFq.r7Vpsm/uApiVs2', NULL, '2019-12-18 09:55:50', '2019-12-18 09:55:50');
+(33, 'Saif Uzzal', 'naylamerrry40720@gmail.com', NULL, '$2y$10$ha7pkm/P2ojLqcXBYdjgyufzpC9RKTqVGV2sexQxrNlcH137MIyWC', NULL, '2019-12-22 03:09:15', '2019-12-22 03:09:15'),
+(36, 'Aakil', 'aakilaslamas@gmail.com', NULL, '$2y$10$gl/aQUN3igXQINWyz47Wa.CzH7gGUCrUnsNoXwIcRFR8f1Zi6bIQy', NULL, '2019-12-23 13:05:00', '2019-12-23 13:05:00');
 
 --
 -- Indexes for dumped tables
@@ -345,13 +374,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `guardianreviews`
 --
 ALTER TABLE `guardianreviews`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `guardians`
 --
 ALTER TABLE `guardians`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -381,19 +410,19 @@ ALTER TABLE `tuitions`
 -- AUTO_INCREMENT for table `tutorreviews`
 --
 ALTER TABLE `tutorreviews`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tutors`
 --
 ALTER TABLE `tutors`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
